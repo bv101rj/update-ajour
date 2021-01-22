65ce027126a62093fa8c99d011d6b997304852cb
bv101rj <robert@janey.tech>
2021-01-22 13:29:02 -0500

    Totally new, also actually works lol

--- update-ajour.sh
+++ update-ajour.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
+!/bin/bash
 rm -rf /home/robb/Builds/ajour
-source /home/robb/Builds/
 git clone https://github.com/casperstorm/ajour.git
-source /home/robb/Builds/ajour
-cargo build --release 
-source /home/robb/Builds/ajour/target/release
-cp ajour /usr/local/bin/ajour
+(cd ajour; cargo build --release)
+(cd ajour/target/release; sudo cp ajour /usr/local/bin/ajour)
